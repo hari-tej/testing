7808a5ee025f506efd789aecb366ff4f43332470
Hari-tej <chegondihariteja@gmail.com>
2023-07-14 12:15:44 +0530

    done again

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div class="card" style={{width: 18 +'rem'}}>
-  <img src="og.jpg" class="card-img-top" alt="image"/>
+  <img src="og.jpg" class="card-img-top" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
