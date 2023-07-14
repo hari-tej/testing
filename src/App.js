170d0367bfcc3811cd948539adab05ebf4929ef3
Hari-tej <chegondihariteja@gmail.com>
2023-07-14 11:44:30 +0530

    testing being done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="card" style={{width: 18+'rem'}}>
+  <img src="og.jpg" class="card-img-top" alt="image"/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="www.google.com" class="btn btn-primary">Go to google</a><br></br>
+    <a href="https://instagram.com/lithium_hydrogen_arsenic?igshid=MzRlODBiNWFlZA==" class="btn btn-primary">Instagram</a><br></br>
+    <button ><a  href='Github.pdf' download='resume.pdf'><b><h3>Download Resume</h3></b></a></button>
+  </div>
+</div>
+       
     </div>
   );
 }
