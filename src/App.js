4d3f889238bcd389f49b7b8ac23a4bd8b2e40660
Hari-tej <chegondihariteja@gmail.com>
2023-07-14 12:07:58 +0530

    final testing

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div class="card" style={{width: 18+'rem'}}>
+      <div class="card" style={{width: 18 +'rem'}}>
   <img src="og.jpg" class="card-img-top" alt="image"/>
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="www.google.com" class="btn btn-primary">Go to google</a><br></br>
     <a href="https://instagram.com/lithium_hydrogen_arsenic?igshid=MzRlODBiNWFlZA==" class="btn btn-primary">Instagram</a><br></br>
-    <button ><a  href='Github.pdf' download='resume.pdf'><b><h3>Download Resume</h3></b></a></button>
+    <button><a href='Github.pdf' download='github.pdf'>Download now</a></button>
   </div>
 </div>
-       
     </div>
   );
 }
