a6ebba926596809a4f325ca2fee0ba9f7c59cb0b
Hari-tej <chegondihariteja@gmail.com>
2023-07-19 18:49:18 +0530

    test number four

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   <img src="og.jpg" class="card-img-top" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
-    <p class="card-text">CHEGODI HARI TEJA IS MY NAME</p>
+    <p class="card-text">OG WILL BE BLOCKBUSTER</p>
     <a href="https://instagram.com/lithium_hydrogen_arsenic?igshid=MzRlODBiNWFlZA==" class="btn btn-primary">Instagram</a><br></br>
     <button><a href='/file/Github.pdf' download='gamping.pdf'>Download now</a></button>
   </div>
